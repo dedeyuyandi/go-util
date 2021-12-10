8611fd465d9c9ba2e489d33fab23317c81949c72
Dede Yuyandi <dede.yuyandy@gmail.com>
2021-12-10 15:38:14 +0700

    first commit

---
+++ query/guery.go
@@ -0,0 +1,26 @@
+package query
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+func Where(request map[string]interface{}) string {
+	var w []string
+	for k, v := range request {
+		switch v.(type) {
+		case uint64, uint32, int32, int64, uint8, uint16, int8, int16, int:
+			w = append(w, fmt.Sprintf("%s = %d", k, v))
+		case float32, float64, bool, uuid.UUID:
+			w = append(w, fmt.Sprintf("%s = %v", k, v))
+		default:
+			w = append(w, fmt.Sprintf("%s = '%s'", k, v))
+		}
+	}
+	if len(w) > 0 {
+		return strings.Join(w, " AND ")
+	}
+	return ""
+}
